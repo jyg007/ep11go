77715a36c1336ac2a4dc649cd76c65064a156378
root <jyg007@gmail.com>
2025-03-25 18:08:38 -0400

    Add rencipher function

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: testcrypto aescreatekey createblskeypair blsaggregatepk blsaggregatesig signbls derivekey derivekeybls createseed uncipherblob createkyberkeypair kyberencapsulate kyberdecapsulate aesencrypt aesdecrypt createdilithkeypair signdilith verifydilith aescreatekeysession verifybls
+all: testcrypto aescreatekey createblskeypair blsaggregatepk blsaggregatesig signbls derivekey derivekeybls createseed uncipherblob createkyberkeypair kyberencapsulate kyberdecapsulate aesencrypt aesdecrypt createdilithkeypair signdilith verifydilith aescreatekeysession verifybls reencipher
 
 deps = ep11/types.go ep11/error.go  ep11/params.go ep11/hsminit.go ep11/ep11.go ep11/constants.go
 
@@ -11,6 +11,9 @@ testcrypto: test.go ep11/types.go ep11/error.go  ep11/params.go ep11/hsminit.go 
 	go build -o testcrypto test.go
 
 aescreatekey: aescreatekey.go
+	go build $^
+
+reencipher: reencipher.go
 	go build $^
 
 aescreatekeysession: aescreatekeysession.go
---
+++ reencipher.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+#cgo LDFLAGS: -lep11
+#cgo CFLAGS: -I/usr/include/ep11 -I/usr/include/opencryptoki
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ep11.h>
+*/
+import "C"
+import "fmt"
+import "ep11go/ep11"
+
+
+//##########################################################################################################################################################################################
+//##########################################################################################################################################################################################
+func main() { 
+      target := ep11.HsmInitNonVirtual("3.19") 
+ 
+      keyTemplate := ep11.Attributes{
+	      C.CKA_VALUE_LEN: 16 ,
+		C.CKA_UNWRAP: false,
+		C.CKA_ENCRYPT: true,
+      }
+
+
+	var aeskey ep11.KeyBlob
+	for {
+       	aeskey, _ = ep11.GenerateKey(target,
+                	ep11.Mech(C.CKM_AES_KEY_GEN, nil),
+	                keyTemplate)
+	fmt.Printf("Generated Key: %x\n", aeskey)
+
+	reenciphered, _ := ep11.Reencipher(target,aeskey)
+	fmt.Printf("Reenciphered Key: %x\n", reenciphered)
+}
+}
