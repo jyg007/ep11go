9426322eeb9ee1c9279f95b3c5e3d4d0a045181e
root <jyg007@gmail.com>
2025-12-09 13:40:50 -0500

    benchmark4 improvment

--- benchmark4.go
+++ benchmark4.go
@@ -8,13 +8,13 @@ package main
 */
 import "C"
 import "fmt"
-import	"strconv"
 import	 "sync"
 import	 "time"
 import "encoding/asn1"
 import "ep11go/ep11"
 import  "crypto/sha256"
-import "os"
+import "flag"
+import "strings" 
 
 type SafeCounter struct {
 	mu sync.Mutex
@@ -22,61 +22,75 @@ type SafeCounter struct {
 }
 
 var COUNTER  SafeCounter
-
+var (
+    threadsFlag = flag.Int("threads", 1, "Number of worker threads")
+    elapsedFlag = flag.Int("elapsed", 5, "Counter interval in seconds")
+    curveFlag   = flag.String("curve", "secp256k1", "Curve to use: secp256k1 or ed25519")
+    cardsFlag   = flag.String("cards", "03.19", "Space-separated list of adapter.domain pairs")
+)
 
 
 func start(c chan int) {
 
-         target := ep11.HsmInit("3.19") 
-	 ecParameters, err := asn1.Marshal(ep11.OIDNamedCurveED25519)
-       //ecParameters, err := asn1.Marshal(ep11.OIDNamedCurveSecp256k1)
+       target := ep11.HsmInit(*cardsFlag)
+ var ecParameters []byte
+    var err error
+    var mechSign []*ep11.Mechanism
+
+    switch *curveFlag {
+    case "secp256k1":
+        // EC: secp256k1
+        ecParameters, err = asn1.Marshal(ep11.OIDNamedCurveSecp256k1)
         if err != nil {
-               panic(fmt.Errorf("Unable to encode parameter OID: %s", err))
+            panic(fmt.Errorf("Unable to encode secp256k1 OID: %s", err))
         }
+        mechSign = ep11.Mech(C.CKM_ECDSA, nil)
 
-	publicKeyECTemplate := ep11.Attributes{
-                    C.CKA_EC_PARAMS:ecParameters,
-                    C.CKA_VERIFY:true,
+    case "ed25519":
+        // ED25519: no EC-params, different mechanism
+        ecParameters, err = asn1.Marshal(ep11.OIDNamedCurveED25519)
+        if err != nil {
+            panic(fmt.Errorf("Unable to encode ed25519 OID: %s", err))
         }
-	privateKeyECTemplate := ep11.Attributes{
-                    C.CKA_SIGN:true,
-                    C.CKA_PRIVATE:true,
-        }
+        mechSign = ep11.Mech(C.CKM_IBM_ED25519_SHA512, nil)
 
-	_, sk , _ := ep11.GenerateKeyPair(target, ep11.Mech(C.CKM_EC_KEY_PAIR_GEN, nil), publicKeyECTemplate,privateKeyECTemplate)
-		if err != nil {
-			panic(fmt.Errorf("Sign error: %s", err))
-		}
+    default:
+        panic("Unknown curve: use --curve secp256k1 or --curve ed25519")
+    }
+
+    // Templates
+    publicKeyECTemplate := ep11.Attributes{
+        C.CKA_EC_PARAMS: ecParameters,
+        C.CKA_VERIFY:    true,
+    }
+    privateKeyECTemplate := ep11.Attributes{
+        C.CKA_SIGN:    true,
+        C.CKA_PRIVATE: true,
+    }
+
+    _, sk, _ := ep11.GenerateKeyPair(
+        target,
+        ep11.Mech(C.CKM_EC_KEY_PAIR_GEN, nil),
+        publicKeyECTemplate,
+        privateKeyECTemplate,
+    )
 
     fmt.Print("+")
 
-
-
-
     for {
+        digest := sha256.Sum256([]byte("This data needs to be signed"))
 
-		// Sign data
+        _, err := ep11.SignSingle(target, mechSign, sk, digest[:])
+        if err != nil {
+            panic(fmt.Errorf("Sign error: %s", err))
+        }
 
-		signData := sha256.Sum256([]byte("This data needs to be signed"))
-//                _, err := ep11.SignSingle(target, ep11.Mech(C.CKM_ECDSA,nil),sk,signData[:])
-                _, err := ep11.SignSingle(target, ep11.Mech(C.CKM_IBM_ED25519_SHA512,nil),sk,signData[:])
+        COUNTER.mu.Lock()
+        COUNTER.nb++
+        COUNTER.mu.Unlock()
+    }
 
-		if err != nil {
-			panic(fmt.Errorf("Sign error: %s", err))
-		}
-
-
- 		//*****************************************************************
-	
-
- 		COUNTER.mu.Lock()
- 		COUNTER.nb++
- 		COUNTER.mu.Unlock()
-        
-
-	}
-	c <- 2
-
+    c <- 2
 }
 
 
@@ -99,31 +113,25 @@ func counter(t int) {
 
 
 func main() {
+    flag.Parse() // must come FIRST
+  if *threadsFlag <= 0 {
+        fmt.Println("Threads must be greater than 0")
+        return
+    }
 
-	var elapsed int = 5
+    if *elapsedFlag <= 0 {
+        fmt.Println("Elapsed time must be greater than 0")
+        return
+    }
+    // Sanity check cards string
+    if strings.TrimSpace(*cardsFlag) == "" {
+        fmt.Println("Cards list cannot be empty")
+        return
+    }
 
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: bencmark <thread nb> <elasped time - optional>")
-		return
-	}
-
-	thread_nb, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("Erreur : La valeur doit être un entier")
-		return
-	}
-
-	if len(os.Args) == 3  {
-		elapsed, err = strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Erreur : La valeur doit être un entier")
-			return
-		}
-	}
-	
-	c := make(chan int)
-	go counter(elapsed)
-	for i:=0;i<thread_nb;i++ {
+	 c := make(chan int)
+	go counter(*elapsedFlag)
+	for i:=0;i<*threadsFlag;i++ {
 		time.Sleep(1*time.Second)
 		go start(c)
 	}
@@ -131,5 +139,5 @@ func main() {
 	fmt.Println("")
 
 	_,_ = <-c
-	fmt.Println("hello")
+	fmt.Println("done")
 }
