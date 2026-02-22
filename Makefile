84ab6dd6edfd433f6360907aa26c070cd16173b7
root <jyg007@gmail.com>
2026-02-22 06:20:36 -0500

    add control points

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: testcrypto aescreatekey createblskeypair blsaggregatepk blsaggregatesig signbls derivekey derivekeybls createseed uncipherblob createkyberkeypair kyberencapsulate kyberdecapsulate aesencrypt aesdecrypt createdilithkeypair signdilith verifydilith aescreatekeysession verifybls reencipher reencipher2 getmech aesreencrypt eccreatekeypair ecdh kyberencapsulatehybrid kyberdecapsulatehybrid benchmark4 readattr kmsapi rsacreatekeypair ecwrap ecunwrap sessionunwrap sessionwrap spkiunwrap readpubblob readprivblob rsaunwrap aesunwrap ecunwrap2 pkfixasn1 getpubfromskblob ep11login ep11admin ep11load ep11zeroify ep11scanmkvp ep11loadrandom ep11mload ep11audit  ep11card
+all: testcrypto aescreatekey createblskeypair blsaggregatepk blsaggregatesig signbls derivekey derivekeybls createseed uncipherblob createkyberkeypair kyberencapsulate kyberdecapsulate aesencrypt aesdecrypt createdilithkeypair signdilith verifydilith aescreatekeysession verifybls reencipher reencipher2 getmech aesreencrypt eccreatekeypair ecdh kyberencapsulatehybrid kyberdecapsulatehybrid benchmark4 readattr kmsapi rsacreatekeypair ecwrap ecunwrap sessionunwrap sessionwrap spkiunwrap readpubblob readprivblob rsaunwrap aesunwrap ecunwrap2 pkfixasn1 getpubfromskblob ep11login ep11admin ep11load ep11zeroify ep11scanmkvp ep11loadrandom ep11mload ep11audit  ep11card ep11getattr
 
 deps = ep11/types.go ep11/error.go  ep11/params.go ep11/hsminit.go ep11/ep11.go ep11/constants.go 
 ep11deps = ep11/types.go ep11/error.go  ep11/params.go ep11/hsminit.go ep11/ep11.go ep11/constants.go ep11/ep11login.go ep11/helpers.go
@@ -165,3 +165,6 @@ ep11audit: ep11audit.go $(ep11deps)
 
 ep11card: ep11card.go $(ep11deps)
 	go build  ep11card.go
+
+ep11getattr: ep11getattr.go $(ep11deps)
+	go build  ep11getattr.go
--- ep11getattr.go
+++ ep11getattr.go
@@ -118,6 +118,96 @@ func AnalysePermissions(perm uint32) {
 }
 
 
+// Map of CP numbers to their names
+var cpNames = map[int]string{
+    0:  "ADD_CPBS",
+    1:  "DELETE_CPBS",
+    2:  "SIGN_ASYMM",
+    3:  "SIGN_SYMM",
+    4:  "SIGVERIFY_SYMM",
+    5:  "ENCRYPT_SYMM",
+    6:  "DECRYPT_ASYMM",
+    7:  "DECRYPT_SYMM",
+    8:  "WRAP_ASYMM",
+    9:  "WRAP_SYMM",
+    10: "UNWRAP_ASYMM",
+    11: "UNWRAP_SYMM",
+    12: "KEYGEN_ASYMM",
+    13: "KEYGEN_SYMM",
+    14: "RETAINKEYS",
+    15: "SKIP_KEYTESTS",
+    16: "NON_ATTRBOUND",
+    17: "MODIFY_OBJECTS",
+    18: "RNG_SEED",
+    19: "ALG_RAW_RSA",
+    20: "ALG_NFIPS2009",
+    21: "ALG_NBSI2009",
+    22: "KEYSZ_HMAC_ANY",
+    23: "KEYSZ_BELOW80BIT",
+    24: "KEYSZ_80BIT",
+    25: "KEYSZ_112BIT",
+    26: "KEYSZ_128BIT",
+    27: "KEYSZ_192BIT",
+    28: "KEYSZ_256BIT",
+    29: "KEYSZ_RSA65536",
+    30: "ALG_RSA",
+    31: "ALG_DSA",
+    32: "ALG_EC",
+    33: "ALG_EC_BPOOLCRV",
+    34: "ALG_EC_NISTCRV",
+    35: "ALG_NFIPS2011",
+    36: "ALG_NBSI2011",
+    37: "USER_SET_TRUSTED",
+    38: "ALG_SKIP_CROSSCHK",
+    39: "WRAP_CRYPT_KEYS",
+    40: "SIGN_CRYPT_KEYS",
+    41: "WRAP_SIGN_KEYS",
+    42: "USER_SET_ATTRBOUND",
+    43: "ALLOW_PASSPHRASE",
+    44: "WRAP_STRONGER_KEY",
+    45: "WRAP_WITH_RAW_SPKI",
+    46: "ALG_DH",
+    47: "DERIVE",
+    48: "ALLOW_NONSESSION",
+    55: "ALG_EC_25519",
+    60: "ALG_EC_SECGCRV",
+    61: "ALG_NBSI2017",
+    62: "EMV",
+    63: "EMV_MIX",
+    64: "CPACF_PK",
+    65: "ALG_PQC",
+    66: "BTC",
+    67: "ECDSA_OTHER",
+    68: "ALG_NFIPS2021",
+    69: "ALG_NFIPS2024",
+    70: "COMPAT_LEGACY_SHA3",
+    71: "DSA_PARAMETER_GEN",
+    72: "DERIVE_NON_AB_KEYS",
+    73: "ALLOW_LOGIN_PRE_F2021",
+    74: "ALG_RSA_OAEP",
+    75: "ALLOW_COMBINED_EXTRACT",
+    76: "ALG_EC_PAIRING_FRIENDLY",
+}
+
+// DecodeCPs returns the names of active CPs in the given []byte
+func DecodeCPs(data []byte) []string {
+    var active []string
+    for i, b := range data {
+        for bit := 0; bit < 8; bit++ {
+            if b&(0x80>>bit) != 0 {
+                cpNum := i*8 + bit
+                if name, ok := cpNames[cpNum]; ok {
+                    active = append(active, name)
+                } else {
+                    active = append(active, fmt.Sprintf("Unknown_CP_%d", cpNum))
+                }
+            }
+        }
+    }
+    return active
+}
+
+
 func adminAttrName(index uint32) string {
     switch index {
     case 1:
@@ -166,28 +256,28 @@ func main() {
         target := ep11.HsmInit(controlDomain) 
 	
 // **********************************************************************************************************************
-// QUERY ATTRIBUTES
+// SCAN DOMAIN ATTRIBUTES
 // **********************************************************************************************************************
-	resp , err = ep11.AdminQuery(target,domain, C.XCP_ADMQ_DOM_ATTRS)        
+	resp , err := ep11.AdminQuery(target,domain, C.XCP_ADMQ_DOM_ATTRS)        
         if err != nil {    
             fmt.Println(err)
         }
 	fmt.Println()
 	fmt.Printf("Domain attributes\n")
 	PrintAdminAttributes(resp.Response)
-*/
-
 
 // **********************************************************************************************************************
 // LIST ADMIN SKIs
 // **********************************************************************************************************************
-	resp , err := ep11.AdminQuery(target,domain, C.XCP_ADMQ_DOMADMIN)        
+/*
+	fmt.Printf("Module admins\n")
+	resp , err = ep11.AdminQuery(target,domain, C.XCP_ADMQ_ADMIN)        
         if err != nil {    
             fmt.Println(err)
         }
 	fmt.Println()
 	printSKIs(resp.Response)
-
+*/
 	 
 // **********************************************************************************************************************
 // SCAN CARD ATTRIBUTES
@@ -201,15 +291,17 @@ func main() {
 	PrintAdminAttributes(resp.Response)
 	
 // **********************************************************************************************************************
-// SCAN DOMAIN ATTRIBUTES
+// SCAN CARD ATTRIBUTES
 // **********************************************************************************************************************
-	resp , err = ep11.AdminQuery(target,domain, C.XCP_ADMQ_DOM_ATTRS)        
+	resp , err = ep11.AdminQuery(target,domain, C.XCP_ADMQ_DOM_CTRLPOINTS)        
         if err != nil {    
             fmt.Println(err)
         }
 	fmt.Println()
-	fmt.Printf("Domain attributes\n")
-	PrintAdminAttributes(resp.Response)
+	fmt.Print("Domain Control Points\n")
+
+    	activeCPs := DecodeCPs(resp.Response)
+    	fmt.Println(activeCPs)
 	
 // **********************************************************************************************************************
 // SCAN MEK MKVPS
