e5a232afe2c81ea36a1d41880a9a35d396c7f9d8
root <jyg007@gmail.com>
2026-03-12 05:31:24 -0400

    ep11scanmvp min max improvment

--- ep11scanmkvp.go
+++ ep11scanmkvp.go
@@ -17,45 +17,55 @@ import (
 )
 
 func main() {
-        if len(os.Args) != 3 {
-                fmt.Fprintf(os.Stderr, "usage: %s <control-domain> <max nb>\n", os.Args[0])
+	if len(os.Args) != 4 {
+                fmt.Fprintf(os.Stderr, "usage: %s <control-domain> <min> <max>\n", os.Args[0])
                 os.Exit(1)
         }
 
         // 1) Control domain (e.g. "3.19")
         controlDomain := os.Args[1]
 
-        // 2) Target domain (e.g. 16)
-        max64, err := strconv.ParseUint(os.Args[2], 10, 32)
-	max := uint32(max64)
+        // 2) Min domain
+        min64, err := strconv.ParseUint(os.Args[2], 10, 32)
         if err != nil {
-                fmt.Fprintf(os.Stderr, "invalid: %v\n", err)
+                fmt.Fprintf(os.Stderr, "invalid min value: %v\n", err)
+                os.Exit(1)
+        }
+        min := uint32(min64)
+
+        // 3) Max domain
+        max64, err := strconv.ParseUint(os.Args[3], 10, 32)
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "invalid max value: %v\n", err)
+                os.Exit(1)
+        }
+        max := uint32(max64)
+        if min > max {
+                fmt.Fprintf(os.Stderr, "min cannot be greater than max\n")
                 os.Exit(1)
         }
 
         target := ep11.HsmInit(controlDomain) 
 
 	
-// **********************************************************************************************************************
-// SCAN DOMAINS
-// **********************************************************************************************************************
-for domain := uint32(0); domain <= max; domain++ {
-    fmt.Printf("\n--- Domain %d | 0x%x ---\n", domain,domain)
-
-    resp, err := ep11.AdminQuery(target, domain, C.XCP_ADMQ_WK)
-    if err != nil {
-        fmt.Printf("Error reading current MEK pattern: %v\n", err)
-    } else {
-        fmt.Printf("Current MEK verification pattern: %x\n", resp.Response)
-    }
-
-    resp, err = ep11.AdminQuery(target, domain, C.XCP_ADMQ_NEXT_WK)
-    if err != nil {
-        fmt.Printf("Error reading next MEK pattern: %v\n", err)
-    } else {
-        fmt.Printf("Next MEK verification pattern:    %x\n", resp.Response)
-    }
-}
-
-	 
+        // **********************************************************************************************************************
+        // SCAN DOMAINS
+        // **********************************************************************************************************************
+        for domain := min; domain <= max; domain++ {
+            fmt.Printf("\n--- Domain %d | 0x%x ---\n", domain,domain)
+        
+            resp, err := ep11.AdminQuery(target, domain, C.XCP_ADMQ_WK)
+            if err != nil {
+                fmt.Printf("Error reading current MEK pattern: %v\n", err)
+            } else {
+                fmt.Printf("Current MEK verification pattern: %x\n", resp.Response)
+            }
+        
+            resp, err = ep11.AdminQuery(target, domain, C.XCP_ADMQ_NEXT_WK)
+            if err != nil {
+                fmt.Printf("Error reading next MEK pattern: %v\n", err)
+            } else {
+                fmt.Printf("Next MEK verification pattern:    %x\n", resp.Response)
+            }
+        }
 }
